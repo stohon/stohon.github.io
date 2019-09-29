4070bb939273c148f4589e3de598af33e4c7e833
Ben Stohon <38965503+stohon@users.noreply.github.com>
2019-09-29 18:17:47 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,7 @@
 var sitePages = [
 { "name": "Home", "url": "index.html" },
 { "name": "About Me", "url": "pages/aboutme.html" },
+{ "name": "References", "url": "pages/references.html" },  
 { "name": "Blog Engine", "url": "pages/blogengine.html" },
 { "name": "Blog Gists", "url": "https://gist.github.com/stohon" }
 ]
